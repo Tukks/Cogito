56ac26e00b13bd8a82d1e5056b88647a032703f8
Giuseppe Lapenta <glapenta@enovacom.fr>
2022-11-01 22:46:10 +0100

    ajout virtual scroll

--- my-thought-front/src/app/folder/folder.page.ts
+++ my-thought-front/src/app/folder/folder.page.ts
@@ -10,14 +10,18 @@ import {CardsLink, CardsType} from "../types/cards-link";
 })
 export class FolderPage implements OnInit {
   public folder: string;
-  public cardsLink: CardsLink[];
+  public cardsLink: CardsLink[] = [];
   public cardsType: typeof CardsType = CardsType;
 
   constructor(private activatedRoute: ActivatedRoute, private thoughtsService: ThoughtsService) {
   }
 
   ngOnInit() {
-    this.thoughtsService.getAllthougts().subscribe(val => this.cardsLink = val);
+    this.thoughtsService.getAllthougts().subscribe(val => {
+      for (let i = 0; i < 100; i++) {
+        this.cardsLink = this.cardsLink.concat(val);
+      }
+    });
     // this.thoughtsService.saveLink("https://twitter.com/Foone/status/1587144406096052226").subscribe(val => this.cardsLink = val);
 
 
