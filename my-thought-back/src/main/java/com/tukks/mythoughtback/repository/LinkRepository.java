95406a4c4f3fff867bb3bbf23f8fb1b6786a4010
Giuseppe Lapenta <glapenta@enovacom.fr>
2022-11-03 18:15:43 +0100

    Replace jpaRepository par CrudRepository

--- my-thought-back/src/main/java/com/tukks/mythoughtback/repository/LinkRepository.java
+++ my-thought-back/src/main/java/com/tukks/mythoughtback/repository/LinkRepository.java
@@ -2,14 +2,14 @@ package com.tukks.mythoughtback.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 import com.tukks.mythoughtback.entity.LinkEntity;
 
 @Service
-public interface LinkRepository extends JpaRepository<LinkEntity, Long> {
+public interface LinkRepository extends CrudRepository<LinkEntity, Long> {
 
 	@Query("select p from LinkEntity p")
 	List<LinkEntity> getAll();
--- my-thought-back/src/main/java/com/tukks/mythoughtback/repository/ThingsRepository.java
+++ my-thought-back/src/main/java/com/tukks/mythoughtback/repository/ThingsRepository.java
@@ -2,14 +2,14 @@ package com.tukks.mythoughtback.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 import com.tukks.mythoughtback.entity.ThingsEntity;
 
 @Service
-public interface ThingsRepository extends JpaRepository<ThingsEntity, Long> {
+public interface ThingsRepository extends CrudRepository<ThingsEntity, Long> {
 
 	@Query("select p from ThingsEntity p")
 	List<Object> getAll();
