80fa729ab2a0bc7609aa07ee36eea394ee5707ae
Giuseppe Lapenta <glapenta@enovacom.fr>
2022-11-07 19:43:41 +0100

    correction des link preview pour prendre en charge plus de cas

--- my-thought-back/src/main/java/com/tukks/mythoughtback/service/internal/LinkPreview.java
+++ my-thought-back/src/main/java/com/tukks/mythoughtback/service/internal/LinkPreview.java
@@ -1,6 +1,7 @@
 package com.tukks.mythoughtback.service.internal;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.apache.commons.lang3.StringUtils;
@@ -35,22 +36,106 @@ public class LinkPreview {
 			Document document = Jsoup.connect(url)
 				.userAgent("Mozilla")
 				.get();
-			String title = getMetaTagContent(document, "meta[name=title]");
-			String desc = getMetaTagContent(document, "meta[name=description]");
-			String ogUrl = StringUtils.defaultIfBlank(getMetaTagContent(document, "meta[property=og:url]"), url);
-			String ogTitle = getMetaTagContent(document, "meta[property=og:title]");
-			String ogDesc = getMetaTagContent(document, "meta[property=og:description]");
-			String ogImage = getMetaTagContent(document, "meta[property=og:image]");
+			String title = getTitle(document);
+			String desc = getDescription(document);
+			String ogUrl = StringUtils.defaultIfBlank(getUrl(document), url);
+			String ogImage = getImg(document, url);
 			String ogImageAlt = getMetaTagContent(document, "meta[property=og:image:alt]");
-			String domain = ogUrl;
 
-			domain = new URL(ogUrl).getHost();
-			return new LinkEntity(domain, url, StringUtils.defaultIfBlank(ogTitle, title), StringUtils.defaultIfBlank(ogDesc, desc), ogImage, ogImageAlt);
+			String domain = new URL(ogUrl).getHost();
+			return new LinkEntity(domain, url, title, desc, ogImage, ogImageAlt);
 
 		} catch (IOException e) {
 			logger.warn("Unable to connect to extract domain name from : {}", url);
 		}
 		return null;
+	}
+
+	private String getTitle(Document document) {
+		var title = getMetaTagContent(document, "meta[property=\"og:title\"]");
+		if (!title.equals(StringUtils.EMPTY)) {
+			return title;
+		}
+		title = getMetaTagContent(document, "meta[name=\"twitter:title\"]");
+		if (!title.equals(StringUtils.EMPTY)) {
+			return title;
+		}
+		title = document.title();
+		if (!title.equals(StringUtils.EMPTY)) {
+			return title;
+		}
+		title = document.select("h1").first().html();
+		if (!title.equals(StringUtils.EMPTY)) {
+			return title;
+		}
+		title = document.select("h2").first().html();
+		if (!title.equals(StringUtils.EMPTY)) {
+			return title;
+		}
+		return "";
+	}
+
+	private String getDescription(Document document) {
+		var description = getMetaTagContent(document, "meta[property=\"og:description\"]");
+		if (!description.equals(StringUtils.EMPTY)) {
+			return description;
+		}
+		description = getMetaTagContent(document, "meta[name=\"twitter:description\"]");
+		if (!description.equals(StringUtils.EMPTY)) {
+			return description;
+		}
+		description = getMetaTagContent(document, "meta[name=\"description\"]");
+		if (!description.equals(StringUtils.EMPTY)) {
+			return description;
+		}
+		Element p = document.select("p").first();
+		if (p != null) {
+			return p.html();
+		}
+		return "";
+	}
+
+	private String getUrl(Document document) {
+		var url = getMetaTagContent(document, "link[rel=canonical]");
+		if (!url.equals(StringUtils.EMPTY)) {
+			return url;
+		}
+		url = getMetaTagContent(document, "meta[property=\"og:url\"]");
+		if (!url.equals(StringUtils.EMPTY)) {
+			return url;
+		}
+		return "";
+	}
+
+	private String getImg(Document document, String url) {
+		var img = getMetaTagContent(document, "meta[property=\"og:image\"]");
+		if (!img.equals(StringUtils.EMPTY)) {
+			return img;
+		}
+		img = getMetaTagContent(document, "link[rel=\"image_src\"]");
+		if (!img.equals(StringUtils.EMPTY)) {
+			return img;
+		}
+		img = getMetaTagContent(document, "meta[name=\"twitter:image\"]");
+		if (!img.equals(StringUtils.EMPTY)) {
+			return img;
+		}
+		Element imgElement = document.select("img").first();
+		if (imgElement != null) {
+			String src = imgElement.attr("src");
+			if (src.contains("//")) {
+				return src;
+			} else {
+				try {
+					URL uri = new URL(url);
+					return uri.getProtocol() + "://" + uri.getHost() + src;
+				} catch (MalformedURLException e) {
+					// Pas dramatique, on a rien besoin de faire avec cette exception
+					return "";
+				}
+			}
+		}
+		return "";
 	}
 
 	/**
