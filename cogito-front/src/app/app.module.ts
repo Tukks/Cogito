dad5c50532ee1826bb0e725656d9dd6699c22d49
Giuseppe Lapenta <glapenta@enovacom.fr>
2022-12-09 14:44:57 +0100

    Clean unused directive

--- cogito-front/src/app/app.module.ts
+++ cogito-front/src/app/app.module.ts
@@ -1,43 +1,42 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BoardComponent } from './page/board/board.component';
-import { CardComponent } from './components/card/card.component';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { MarkdownCardComponent } from './components/markdown-card/markdown-card.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ModalEditorComponent } from './components/modal-editor/modal-editor.component';
-import { DialogModule } from '@angular/cdk/dialog';
-import { fr_FR, NZ_I18N } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import fr from '@angular/common/locales/fr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { CustomTagsInput } from './components/custom-tags-input';
-import { NzTagModule } from 'ng-zorro-antd/tag';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzNoAnimationModule } from 'ng-zorro-antd/core/no-animation';
-import { NzTypographyModule } from 'ng-zorro-antd/typography';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { LayoutModule } from '@angular/cdk/layout';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { HandleShareComponent } from './page/handle-share/handle-share.component';
-import { HttpErrorInterceptor } from './http-error.interceptor';
-import { NzMessageModule } from 'ng-zorro-antd/message';
-import { ImgFallbackDirective } from './directive/img-fallback.directive';
-import { IntersectionObserverElementDirective } from './internal-service/intersection-observser/intersection-observer-element.directive';
-import { IntersectionObserverGridDirective } from './internal-service/intersection-observser/intersection-observer-grid.directive';
-import { LoginComponent } from './page/login/login.component';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-import { ClipboardModule } from '@angular/cdk/clipboard';
-import { RegisterComponent } from './page/register/register.component';
-import { NzAffixModule } from 'ng-zorro-antd/affix';
-import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BoardComponent } from "./page/board/board.component";
+import { CardComponent } from "./components/card/card.component";
+import { ScrollingModule } from "@angular/cdk/scrolling";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { MarkdownCardComponent } from "./components/markdown-card/markdown-card.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ModalEditorComponent } from "./components/modal-editor/modal-editor.component";
+import { DialogModule } from "@angular/cdk/dialog";
+import { fr_FR, NZ_I18N } from "ng-zorro-antd/i18n";
+import { registerLocaleData } from "@angular/common";
+import fr from "@angular/common/locales/fr";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NzInputModule } from "ng-zorro-antd/input";
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { CustomTagsInput } from "./components/custom-tags-input";
+import { NzTagModule } from "ng-zorro-antd/tag";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { NzNoAnimationModule } from "ng-zorro-antd/core/no-animation";
+import { NzTypographyModule } from "ng-zorro-antd/typography";
+import { NzFormModule } from "ng-zorro-antd/form";
+import { LayoutModule } from "@angular/cdk/layout";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import { HandleShareComponent } from "./page/handle-share/handle-share.component";
+import { HttpErrorInterceptor } from "./http-error.interceptor";
+import { NzMessageModule } from "ng-zorro-antd/message";
+import { IntersectionObserverElementDirective } from "./internal-service/intersection-observser/intersection-observer-element.directive";
+import { IntersectionObserverGridDirective } from "./internal-service/intersection-observser/intersection-observer-grid.directive";
+import { LoginComponent } from "./page/login/login.component";
+import { NzDropDownModule } from "ng-zorro-antd/dropdown";
+import { ClipboardModule } from "@angular/cdk/clipboard";
+import { RegisterComponent } from "./page/register/register.component";
+import { NzAffixModule } from "ng-zorro-antd/affix";
+import { NzRadioModule } from "ng-zorro-antd/radio";
 
 registerLocaleData(fr);
 
@@ -51,7 +50,6 @@ registerLocaleData(fr);
     CustomTagsInput,
     HandleShareComponent,
     LoginComponent,
-    ImgFallbackDirective,
     IntersectionObserverGridDirective,
     IntersectionObserverElementDirective,
     RegisterComponent,
--- cogito-front/src/app/directive/img-fallback.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import {ImgFallbackDirective} from './img-fallback.directive';
-
-describe('ImgFallbackDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ImgFallbackDirective();
-    expect(directive).toBeTruthy();
-  });
-});
